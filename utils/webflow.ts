87ec553c4fba86df6ed261bd9c11dea24e38e58e
Alex Iglesias <alexiglesias@finsweet.com>
2021-09-06 12:41:56 +0200

    Update webflow.ts

--- utils/webflow.ts
+++ utils/webflow.ts
@@ -2,4 +2,7 @@ export const WEBFLOW_CSS_CLASSES = {
   slider: 'w-slider',
   slide: 'w-slide',
   sliderDot: 'w-slider-dot',
+  collectionWrapper: 'w-dyn-list',
+  collectionList: 'w-dyn-items',
+  collectionItem: 'w-dyn-item',
 } as const;
