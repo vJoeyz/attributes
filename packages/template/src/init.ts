75ea01a2b3de6040d2767b13672d3f84132d067e
Alex Iglesias <alexiglesias@finsweet.com>
2021-09-25 12:19:57 +0200

    Update init.ts

--- packages/template/src/init.ts
+++ packages/template/src/init.ts
@@ -2,6 +2,15 @@ interface Params {
   param?: unknown;
 }
 
+/**
+ * Inits the attribute.
+ *
+ * Auto init:
+ * @param params The current `<script>` element.
+ *
+ * Programatic init:
+ * @param params.param A global parameter.
+ */
 export const init = (params?: HTMLOrSVGScriptElement | Params | null): void => {
   if (params instanceof HTMLScriptElement || params instanceof SVGScriptElement) {
     console.log(params);
