a515cfe6ec0328cb4c1349adabd0d2e984c04b76
Alex Iglesias <alexiglesias@finsweet.com>
2021-10-20 16:45:41 +0200

    `populate`: Added populating HTMLSelect elements

--- packages/populate/src/select.ts
+++ packages/populate/src/select.ts
@@ -1,9 +1,21 @@
 import { getInstanceIndex } from '$utils/attributes';
 import { ATTRIBUTES, getSelector } from './constants';
 
+/**
+ * Populates a Select element's options based on the source values.
+ * @param selectElement
+ */
 export const populateSelectElement = (selectElement: HTMLSelectElement) => {
   const instanceIndex = getInstanceIndex(selectElement, ATTRIBUTES.element.key);
 
   const displaySources = document.querySelectorAll<HTMLElement>(getSelector('element', 'source', { instanceIndex }));
-  if (!displaySources) return;
+  if (!displaySources.length) return;
+
+  for (const displaySource of displaySources) {
+    const { innerText } = displaySource;
+    if (!innerText) continue;
+
+    const newOption = new Option(innerText, innerText);
+    selectElement.options.add(newOption);
+  }
 };
