17adf05841f4be7bd2a4e8f3d2357eb153a7ff42
Alex Iglesias <alexiglesias@finsweet.com>
2021-10-28 11:22:41 +0200

    `animation`: Fixed missing `null` type on `anchor`

--- packages/animation/src/types.ts
+++ packages/animation/src/types.ts
@@ -11,7 +11,7 @@ export interface AnimationProps {
 type AnimationOptions = Pick<AnimationListOptions, 'duration' | 'easing'> & { stagger?: number };
 type AnimationBase<T> = (element: HTMLElement | HTMLElement[], options?: T & AnimationOptions) => Promise<void>;
 
-type AnimationIn = AnimationBase<{ target?: Element; anchor?: Element }>;
+type AnimationIn = AnimationBase<{ target?: Element; anchor?: Element | null }>;
 type AnimationOut = AnimationBase<{ remove?: boolean }>;
 
 export type AnimationFunctions = { animateIn: AnimationIn; animateOut: AnimationOut };
