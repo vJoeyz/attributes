b291120703e6e44314f6ba1d4368ec79132ba80a
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 10:54:23 +0100

    `cmscombine`: Created `changesets.json` API.

---
+++ packages/cms/cmscombine/api/changesets.ts
@@ -0,0 +1,56 @@
+import type { PartialAttributeChangesets } from '$utils/types/changesets';
+
+export const changesets: PartialAttributeChangesets = [
+  {
+    version: 'v1.3.1',
+    date: '17 November 2021',
+  },
+  {
+    version: 'v1.3.0',
+    date: '17 November 2021',
+  },
+  {
+    version: 'v1.2.4',
+    date: '16 November 2021',
+  },
+  {
+    version: 'v1.2.3',
+    date: '16 November 2021',
+  },
+  {
+    version: 'v1.2.2',
+    date: '11 November 2021',
+  },
+  {
+    version: 'v1.2.1',
+    date: '11 November 2021',
+  },
+  {
+    version: 'v1.2.0',
+    date: '11 November 2021',
+  },
+  {
+    version: 'v1.1.3',
+    date: '10 November 2021',
+  },
+  {
+    version: 'v1.1.2',
+    date: '10 November 2021',
+  },
+  {
+    version: 'v1.1.1',
+    date: '10 November 2021',
+  },
+  {
+    version: 'v1.1.0',
+    date: '23 October 2021',
+  },
+  {
+    version: 'v1.0.1',
+    date: '27 September 2021',
+  },
+  {
+    version: 'v1.0.0',
+    date: '26 September 2021',
+  },
+];
