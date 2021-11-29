fdd046cfe896c55b63033fc42fdc5bce9459e30e
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 12:40:32 +0100

    `displayvalues`: Created `changesets.json` API.

--- packages/displayvalues/README.md
+++ packages/displayvalues/README.md
@@ -1,3 +1,10 @@
 # `displayvalues` Attribute
 
 Display the value of a Form Element.
+
+## CDN Import
+
+```html
+<!-- [Attributes by Finsweet] CMS Display Values -->
+<script defer src="https://cdn.jsdelivr.net/npm/@finsweet/attributes-displayvalues@1/displayvalues.js"></script>
+```
---
+++ packages/displayvalues/api/changesets.ts
@@ -0,0 +1,20 @@
+import type { PartialAttributeChangesets } from '$utils/types/changesets';
+
+export const changesets: PartialAttributeChangesets = [
+  {
+    version: 'v1.1.0',
+    date: '29 November 2021',
+  },
+  {
+    version: 'v1.0.2',
+    date: '25 October 2021',
+  },
+  {
+    version: 'v1.0.1',
+    date: '20 October 2021',
+  },
+  {
+    version: 'v1.0.0',
+    date: '19 October 2021',
+  },
+];
