3e5fe63a222f6854a8a20ba197ba2436387a0b8f
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 13:24:40 +0100

    `untransform`: Created `changesets.json` API.

--- packages/untransform/README.md
+++ packages/untransform/README.md
@@ -1,3 +1,10 @@
 # `untransform` Attribute
 
-Attribute description.
+Untransform all parents of a fixed element to prevent unwanted positioning behaviors.
+
+## CDN Import
+
+```html
+<!-- [Attributes by Finsweet] CMS Fixed Position Untransform -->
+<script defer src="https://cdn.jsdelivr.net/npm/@finsweet/attributes-untransform@1/untransform.js"></script>
+```
---
+++ packages/untransform/api/changesets.ts
@@ -0,0 +1,24 @@
+import type { PartialAttributeChangesets } from '$utils/types/changesets';
+
+export const changesets: PartialAttributeChangesets = [
+  {
+    version: 'v1.1.0',
+    date: '29 November 2021',
+  },
+  {
+    version: 'v1.0.8',
+    date: '25 October 2021',
+  },
+  {
+    version: 'v1.0.7',
+    date: '16 October 2021',
+  },
+  {
+    version: 'v1.0.6',
+    date: '25 September 2021',
+  },
+  {
+    version: 'v1.0.0',
+    date: '25 September 2021',
+  },
+];
