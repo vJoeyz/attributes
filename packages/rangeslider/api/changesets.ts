9458f36a992325931aa3ae5ed95014f4fd5cc0b9
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 13:06:02 +0100

    `rangeslider`: Created `changesets.json` API.

---
+++ packages/rangeslider/api/changesets.ts
@@ -0,0 +1,72 @@
+import type { PartialAttributeChangesets } from '$utils/types/changesets';
+
+export const changesets: PartialAttributeChangesets = [
+  {
+    version: 'v1.3.0',
+    date: '29 November 2021',
+  },
+  {
+    version: 'v1.2.0',
+    date: '28 November 2021',
+  },
+  {
+    version: 'v1.2.0-beta.0',
+    date: '27 November 2021',
+  },
+  {
+    version: 'v1.1.2',
+    date: '15 November 2021',
+  },
+  {
+    version: 'v1.1.1',
+    date: '15 November 2021',
+  },
+  {
+    version: 'v1.1.0',
+    date: '15 November 2021',
+  },
+  {
+    version: 'v1.0.1',
+    date: '15 November 2021',
+  },
+  {
+    version: 'v1.0.0',
+    date: '15 November 2021',
+  },
+  {
+    version: 'v1.0.0-beta.8',
+    date: '14 November 2021',
+  },
+  {
+    version: 'v1.0.0-beta.7',
+    date: '14 November 2021',
+  },
+  {
+    version: 'v1.0.0-beta.6',
+    date: '14 November 2021',
+  },
+  {
+    version: 'v1.0.0-beta.5',
+    date: '14 November 2021',
+  },
+  {
+    version: 'v1.0.0-beta.4',
+    date: '13 November 2021',
+  },
+  {
+    version: 'v1.0.0-beta.3',
+    date: '13 November 2021',
+  },
+  {
+    version: 'v1.0.0-beta.2',
+    date: '13 November 2021',
+  },
+  {
+    version: 'v1.0.0-beta.1',
+    date: '13 November 2021',
+  },
+  {
+    version: 'v1.0.0-beta.0',
+    date: '13 October 2021',
+  },
+];
