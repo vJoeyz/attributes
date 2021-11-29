66b94f02bea48f4d1b24814874ce4aa3b0067dd7
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 10:47:39 +0100

    `autovideo`: Created `changesets.json` API.

---
+++ packages/autovideo/api/changesets.ts
@@ -0,0 +1,20 @@
+import type { PartialAttributeChangesets } from '$utils/types/changesets';
+
+export const changesets: PartialAttributeChangesets = [
+  {
+    version: 'v1.1.0',
+    date: '29 November 2021',
+  },
+  {
+    version: 'v1.0.2',
+    date: '17 November 2021',
+  },
+  {
+    version: 'v1.0.1',
+    date: '6 November 2021',
+  },
+  {
+    version: 'v1.0.0',
+    date: '6 November 2021',
+  },
+];
