65c814d35da7e0530a6195f5c16b809d5f76f5c0
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 11:06:36 +0100

    `cmscore`: Created `changesets.json` API

--- packages/cms/cmscore/bin/build.js
+++ packages/cms/cmscore/bin/build.js
@@ -1,6 +1,11 @@
 // Import ESBuild
 import esbuild from 'esbuild';
 import dotenv from 'dotenv';
+import { dirname } from 'path';
+import { writeFileSync, readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config({ path: '../../../.env' });
 
@@ -23,4 +28,30 @@ esbuild.build({
   entryPoints: ['src/index.ts'],
   outfile: `${production ? '' : process.env.CUSTOM_BUILD_DIRECTORY || ''}/cmscore.js`,
   format: 'esm',
+});
+
+esbuild.buildSync({
+  ...defaultSettings,
+  entryPoints: ['api/changesets.ts'],
+  outfile: 'temp/changesets.js',
+  format: 'esm',
+});
+
+/**
+ * Changeset JSON generation
+ */
+import('../temp/changesets.js').then(({ changesets }) => {
+  const fullChangesets = changesets.map((changeset) => {
+    const { version } = changeset;
+
+    const markdown = readFileSync(`${__dirname}/../.changesets/${version}.md`, 'utf-8');
+    if (!markdown) throw new Error(`File ${version}.md doesn't exist.`);
+
+    return {
+      ...changeset,
+      markdown,
+    };
+  });
+
+  writeFileSync(`${__dirname}/../changesets.json`, JSON.stringify(fullChangesets));
 });
