9015a0e23e44c7e52849ede84deaa4dc20f37b31
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 13:16:14 +0100

    `scrolldisable`: Created `changesets.json` API.

---
+++ packages/scrolldisable/api/changesets.ts
@@ -0,0 +1,40 @@
+import type { PartialAttributeChangesets } from '$utils/types/changesets';
+
+export const changesets: PartialAttributeChangesets = [
+  {
+    version: 'v1.2.0',
+    date: '29 November 2021',
+  },
+  {
+    version: 'v1.1.11',
+    date: '25 November 2021',
+  },
+  {
+    version: 'v1.1.10',
+    date: '16 November 2021',
+  },
+  {
+    version: 'v1.1.9',
+    date: '25 October 2021',
+  },
+  {
+    version: 'v1.1.8',
+    date: '8 October 2021',
+  },
+  {
+    version: 'v1.1.7',
+    date: '27 September 2021',
+  },
+  {
+    version: 'v1.1.6',
+    date: '25 September 2021',
+  },
+  {
+    version: 'v1.1.5',
+    date: '25 September 2021',
+  },
+  {
+    version: 'v1.0.0',
+    date: '25 September 2021',
+  },
+];
