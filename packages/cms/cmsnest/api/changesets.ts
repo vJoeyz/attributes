9e6370f6c6d35c5cca4ae902de45b8b2f598fd93
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 11:52:00 +0100

    `cmsnest`: Created `changesets.json` API.

---
+++ packages/cms/cmsnest/api/changesets.ts
@@ -0,0 +1,40 @@
+import type { PartialAttributeChangesets } from '$utils/types/changesets';
+
+export const changesets: PartialAttributeChangesets = [
+  {
+    version: 'v1.2.0',
+    date: '29 November 2021',
+  },
+  {
+    version: 'v1.1.1',
+    date: '17 November 2021',
+  },
+  {
+    version: 'v1.1.0',
+    date: '17 November 2021',
+  },
+  {
+    version: 'v1.0.5',
+    date: '16 November 2021',
+  },
+  {
+    version: 'v1.0.4',
+    date: '16 November 2021',
+  },
+  {
+    version: 'v1.0.3',
+    date: '11 November 2021',
+  },
+  {
+    version: 'v1.0.2',
+    date: '11 November 2021',
+  },
+  {
+    version: 'v1.0.1',
+    date: '10 November 2021',
+  },
+  {
+    version: 'v1.0.0',
+    date: '10 October 2021',
+  },
+];
