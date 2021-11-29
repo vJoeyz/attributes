e21fb6809773290fbc3c0d3aced9093781f7d19a
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 12:58:50 +0100

    `mirrorinput`: Created `changesets.json` API.

--- packages/mirrorinput/README.md
+++ packages/mirrorinput/README.md
@@ -1,3 +1,10 @@
 # `mirrorinput` Attribute
 
-Attribute description.
+Mirror a user input to another input.
+
+## CDN Import
+
+```html
+<!-- [Attributes by Finsweet] CMS Mirror Input Values -->
+<script defer src="https://cdn.jsdelivr.net/npm/@finsweet/attributes-mirrorinput@1/mirrorinput.js"></script>
+```
---
+++ packages/mirrorinput/api/changesets.ts
@@ -0,0 +1,24 @@
+import type { PartialAttributeChangesets } from '$utils/types/changesets';
+
+export const changesets: PartialAttributeChangesets = [
+  {
+    version: 'v1.1.0',
+    date: '29 November 2021',
+  },
+  {
+    version: 'v1.0.3',
+    date: '25 October 2021',
+  },
+  {
+    version: 'v1.0.2',
+    date: '8 October 2021',
+  },
+  {
+    version: 'v1.0.1',
+    date: '25 September 2021',
+  },
+  {
+    version: 'v1.0.0',
+    date: '25 September 2021',
+  },
+];
