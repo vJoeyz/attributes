3496035e40b2afe92f5f4514599f1a4a4f4ba23d
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 12:54:31 +0100

    `mirrorclick`: Created `changesets.json` API.

--- packages/mirrorclick/bin/build.js
+++ packages/mirrorclick/bin/build.js
@@ -1,8 +1,13 @@
 // Import ESBuild
 import esbuild from 'esbuild';
 import dotenv from 'dotenv';
+import { dirname } from 'path';
+import { writeFileSync, readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 
 dotenv.config({ path: '../../.env' });
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const production = process.env.NODE_ENV === 'production';
 
@@ -22,4 +27,58 @@ esbuild.build({
   ...defaultSettings,
   entryPoints: ['src/index.ts'],
   outfile: `${production ? '' : process.env.CUSTOM_BUILD_DIRECTORY || ''}/mirrorclick.js`,
+});
+
+esbuild.buildSync({
+  ...defaultSettings,
+  entryPoints: ['api/examples.ts'],
+  outfile: 'temp/examples.js',
+  format: 'esm',
+});
+
+esbuild.buildSync({
+  ...defaultSettings,
+  entryPoints: ['api/schema.ts'],
+  outfile: 'temp/schema.js',
+  format: 'esm',
+});
+
+esbuild.buildSync({
+  ...defaultSettings,
+  entryPoints: ['api/changesets.ts'],
+  outfile: 'temp/changesets.js',
+  format: 'esm',
+});
+
+/**
+ * Examples generation
+ */
+import('../temp/examples.js').then(({ examples }) => {
+  writeFileSync(`${__dirname}/../examples.json`, JSON.stringify(examples));
+});
+
+/**
+ * Schema generation
+ */
+import('../temp/schema.js').then(({ schema }) => {
+  writeFileSync(`${__dirname}/../schema.json`, JSON.stringify(schema));
+});
+
+/**
+ * Changeset JSON generation
+ */
+import('../temp/changesets.js').then(({ changesets }) => {
+  const fullChangesets = changesets.map((changeset) => {
+    const { version } = changeset;
+
+    const markdown = readFileSync(`${__dirname}/../.changesets/${version}.md`, 'utf-8');
+    if (!markdown) throw new Error(`File ${version}.md doesn't exist.`);
+
+    return {
+      ...changeset,
+      markdown,
+    };
+  });
+
+  writeFileSync(`${__dirname}/../changesets.json`, JSON.stringify(fullChangesets));
 });
