c05d9b51d678f25a6a27170c61248532f79490f0
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 12:50:09 +0100

    `linkblockedit`: Created `changesets.json` API.

---
+++ packages/linkblockedit/api/changesets.ts
@@ -0,0 +1,32 @@
+import type { PartialAttributeChangesets } from '$utils/types/changesets';
+
+export const changesets: PartialAttributeChangesets = [
+  {
+    version: 'v1.1.0',
+    date: '29 November 2021',
+  },
+  {
+    version: 'v1.0.5',
+    date: '25 October 2021',
+  },
+  {
+    version: 'v1.0.4',
+    date: '8 October 2021',
+  },
+  {
+    version: 'v1.0.3',
+    date: '27 September 2021',
+  },
+  {
+    version: 'v1.0.2',
+    date: '25 September 2021',
+  },
+  {
+    version: 'v1.0.1',
+    date: '25 September 2021',
+  },
+  {
+    version: 'v1.0.0',
+    date: '25 September 2021',
+  },
+];
