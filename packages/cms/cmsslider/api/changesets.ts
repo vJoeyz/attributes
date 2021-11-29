3eca93fa6f93924b6c59ceebfff3941175186cf0
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-29 12:12:44 +0100

    `cmsslider`: Created `changesets.json` API.

---
+++ packages/cms/cmsslider/api/changesets.ts
@@ -0,0 +1,40 @@
+import type { PartialAttributeChangesets } from '$utils/types/changesets';
+
+export const changesets: PartialAttributeChangesets = [
+  {
+    version: 'v1.2.0',
+    date: '19 November 2021',
+  },
+  {
+    version: 'v1.1.2',
+    date: '23 November 2021',
+  },
+  {
+    version: 'v1.1.1',
+    date: '17 November 2021',
+  },
+  {
+    version: 'v1.1.0',
+    date: '17 November 2021',
+  },
+  {
+    version: 'v1.0.4',
+    date: '16 November 2021',
+  },
+  {
+    version: 'v1.0.3',
+    date: '16 November 2021',
+  },
+  {
+    version: 'v1.0.2',
+    date: '11 November 2021',
+  },
+  {
+    version: 'v1.0.1',
+    date: '11 November 2021',
+  },
+  {
+    version: 'v1.0.0',
+    date: '27 September 2021',
+  },
+];
