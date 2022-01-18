10e5ca2fa16ad4c4d3dddfbe837e06ebbb9702d7
Alex Iglesias <alexiglesias@finsweet.com>
2022-01-18 20:29:21 +0100

    Fixed typo

--- packages/cms/cmstabs/api/changesets.ts
+++ packages/cms/cmstabs/api/changesets.ts
@@ -1,7 +1,7 @@
 import type { PartialAttributeChangesets } from '$global/types/changesets';
 
 export const changesets: PartialAttributeChangesets = [
-  { version: 'v1.3.1', date: '18 January 2021' },
+  { version: 'v1.3.1', date: '18 January 2022' },
   { version: 'v1.3.0', date: '19 December 2021' },
   { version: 'v1.2.0', date: '29 November 2021' },
   { version: 'v1.1.2', date: '23 November 2021' },
