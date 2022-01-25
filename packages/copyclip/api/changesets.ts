33485b1d2eb3e6cdd3a49c64be395ffa1022ebce
Alex Iglesias <alexiglesias@finsweet.com>
2022-01-25 11:37:45 +0100

    Published `copyclip` beta.

--- packages/copyclip/api/changesets.ts
+++ packages/copyclip/api/changesets.ts
@@ -1,7 +1,7 @@
 import type { PartialAttributeChangesets } from '$global/types/changesets';
 
 export const changesets: PartialAttributeChangesets = [
-  { version: 'v1.3.0', date: '24 January 2022' },
+  { version: 'v1.3.0-beta.0', date: '24 January 2022' },
   { version: 'v1.2.1', date: '10 December 2021' },
   { version: 'v1.2.0', date: '29 November 2021' },
   { version: 'v1.1.10', date: '25 October 2021' },
