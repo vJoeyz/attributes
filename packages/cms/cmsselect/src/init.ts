09c4651f5c92b5d079c837809c5f96c26aa836df
Alex Iglesias <alexiglesias@finsweet.com>
2022-02-10 17:21:03 +0100

    `cmsselect`: Deployed new init.

--- packages/cms/cmsselect/src/init.ts
+++ packages/cms/cmsselect/src/init.ts
@@ -1,14 +1,18 @@
-import { getSelector } from './constants';
+import { ATTRIBUTE, getSelector } from './constants';
 import { populateSelectElement } from './populate';
 
 /**
  * Inits the attribute.
  *
  */
-export const init = (): void => {
+export const init = (): NodeListOf<Element> => {
   const targetElements = document.querySelectorAll(getSelector('element', 'select', { operator: 'prefixed' }));
 
   for (const targetElement of targetElements) {
     if (targetElement instanceof HTMLSelectElement) populateSelectElement(targetElement);
   }
+
+  window.fsAttributes[ATTRIBUTE].resolve?.(targetElements);
+
+  return targetElements;
 };
