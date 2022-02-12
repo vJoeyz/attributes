5bf278914284168753eeb1bd9600a0f54439cfc3
Alex Iglesias <alexiglesias@finsweet.com>
2022-02-12 17:40:24 +0100

    `selectcustom`: Deployed new init

--- packages/selectcustom/src/index.ts
+++ packages/selectcustom/src/index.ts
@@ -1,6 +1,7 @@
 import { assessScript } from '$global/factory/assess';
 import { initAttributes } from '$global/factory/init';
 
+import { version } from '../package.json';
 import { init } from './init';
 import { ATTRIBUTE } from './utils/constants';
 
@@ -9,10 +10,14 @@ import { ATTRIBUTE } from './utils/constants';
  */
 initAttributes();
 
-const { currentScript } = document;
-const { preventsLoad } = assessScript(currentScript);
+window.fsAttributes[ATTRIBUTE] ||= {};
 
-if (preventsLoad) window.fsAttributes[ATTRIBUTE] = { init };
+const { preventsLoad } = assessScript();
+const attribute = window.fsAttributes[ATTRIBUTE];
+
+attribute.version = version;
+
+if (preventsLoad) attribute.init = init;
 else {
   window.Webflow ||= [];
   window.Webflow.push(init);
