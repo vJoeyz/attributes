6e15b3351a1c61f9e382bd909974340cec1ada13
Alex Iglesias <alexiglesias@finsweet.com>
2022-07-04 08:54:15 +0200

    `formsubmit`: Fixed the wait text not being correctly displayed on the Submit buttons while using enhanced mode.

---
+++ .changeset/fuzzy-geckos-shake.md
@@ -0,0 +1,5 @@
+---
+"@finsweet/attributes-formsubmit": patch
+---
+
+Fixed the wait text not being correctly displayed on the Submit buttons while using enhanced mode.
--- packages/formsubmit/src/utils/dom.ts
+++ packages/formsubmit/src/utils/dom.ts
@@ -25,7 +25,7 @@ export const checkFormSuccess = ({ successMessage, errorMessage }: Form): Promis
  * Displays the Error Message of a Webflow form.
  * @param formInstance
  */
-export const displayFormError = ({ errorMessage }: Form) => {
+export const showFormError = ({ errorMessage }: Form) => {
   errorMessage.style.display = 'block';
 };
 
@@ -33,7 +33,7 @@ export const displayFormError = ({ errorMessage }: Form) => {
  * Hides the Error Message of a Webflow form.
  * @param formInstance
  */
-export const showFormError = ({ errorMessage }: Form) => {
+export const hideFormError = ({ errorMessage }: Form) => {
   errorMessage.style.display = 'none';
 };
 
@@ -46,4 +46,31 @@ export const showFormSuccess = ({ form, successMessage, errorMessage }: Form) =>
   form.style.display = 'none';
   errorMessage.style.display = 'none';
   successMessage.style.display = 'block';
+};
+
+/**
+ * Displays the wait text of a Form's submit buttons.
+ *
+ * @param submitButtons
+ * @returns A callback to reset all texts back to original.
+ */
+export const displayFormSubmitWaitText = ({ submitButtons }: Form) => {
+  const resetCallbacks = submitButtons.reduce<Array<() => void>>((acc, submitButton) => {
+    const { value, dataset } = submitButton;
+    const waitText = dataset.wait;
+
+    if (!value || !waitText) return acc;
+
+    submitButton.value = waitText;
+
+    acc.push(() => {
+      submitButton.value = value;
+    });
+
+    return acc;
+  }, []);
+
+  return () => {
+    for (const resetCallback of resetCallbacks) resetCallback();
+  };
 };
