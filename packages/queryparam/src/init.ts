69f047d45aabf25a6f1b2cc02787abb676c4f377
Mauricio Piber <mauriciopiber@gmail.com>
2022-07-22 18:02:03 -0300

    `queryparam` - Added `cmsfilter` dependency and prevent `removeQuery` if `cmsfilter` enabled

--- packages/queryparam/src/init.ts
+++ packages/queryparam/src/init.ts
@@ -1,10 +1,12 @@
+import { ATTRIBUTE as CMS_FILTER_ATTRIBUTE } from '@finsweet/attributes-cmsfilter/src/utils/constants';
+
 import { queryParamFactory } from './factory';
 import { ATTRIBUTES } from './utils/constants';
 
 /**
  * Inits the attribute.
  */
-export const init = (): void => {
+export const init = async (): Promise<void> => {
   const url = new URL(window.location.href);
 
   for (const [paramKey, paramValue] of [...url.searchParams]) {
@@ -15,6 +17,10 @@ export const init = (): void => {
     }
 
     queryParamFactory(queryParamElements, paramValue);
+
+    if (window.fsAttributes[CMS_FILTER_ATTRIBUTE]) {
+      continue;
+    }
 
     const removeParamFromUrl = queryParamElements.some((element) => {
       return element.hasAttribute(ATTRIBUTES.removeQuery.key);
