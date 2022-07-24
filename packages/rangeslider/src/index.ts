3070846892ee8bb6d11a7489fc63e31eb9d3fd3f
Mauricio Piber <mauriciopiber@gmail.com>
2022-07-23 21:48:17 -0300

    `rangeslider` - Replace local `ATTRIBUTE` by `global/constants`

--- packages/rangeslider/src/index.ts
+++ packages/rangeslider/src/index.ts
@@ -1,18 +1,18 @@
+import { RANGE_SLIDER_ATTRIBUTE } from '@global/constants/attributes';
 import { assessScript, initAttributes } from '@global/factory';
 
 import { version } from '../package.json';
 import { init } from './init';
-import { ATTRIBUTE } from './utils/constants';
 
 /**
  * Init
  */
 initAttributes();
 
-window.fsAttributes[ATTRIBUTE] ||= {};
+window.fsAttributes[RANGE_SLIDER_ATTRIBUTE] ||= {};
 
 const { preventsLoad } = assessScript();
-const attribute = window.fsAttributes[ATTRIBUTE];
+const attribute = window.fsAttributes[RANGE_SLIDER_ATTRIBUTE];
 
 attribute.version = version;
 
