298d42d7c529a7daa57893885ef5bb031e926ede
Mauricio Piber <mauriciopiber@gmail.com>
2022-07-24 22:04:54 -0300

    `cmsattribute` - Added scope for CMS Item without `cmsload` dependencies.

--- packages/cmsattribute/src/init.ts
+++ packages/cmsattribute/src/init.ts
@@ -1,3 +1,4 @@
+import { CMS_CSS_CLASSES } from '@finsweet/ts-utils';
 import { CMS_LOAD_ATTRIBUTE } from 'global/constants/attributes';
 import type { CMSList } from 'packages/cmscore/src';
 
@@ -13,7 +14,9 @@ export const init = async (): Promise<void> => {
   ];
 
   for (const targetElement of targets) {
-    createCMSAttribute(targetElement, null);
+    const cmsListItem = targetElement.closest<HTMLElement>(`.${CMS_CSS_CLASSES.item}`) || null;
+
+    createCMSAttribute(targetElement, cmsListItem);
   }
 
   const listInstances: CMSList[] = (await window.fsAttributes[CMS_LOAD_ATTRIBUTE]?.loading) || [];
