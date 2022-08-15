1c9ebf7b59fdb71637f39de61b7b9c8c36858a0e
Alex Iglesias <alexiglesias@finsweet.com>
2022-08-15 20:29:36 +0200

    Updated `assessScript()` to parse custom `<script>` Attributes

--- global/factory/assess.ts
+++ global/factory/assess.ts
@@ -1,23 +1,36 @@
-import { Debug } from '@finsweet/ts-utils';
-
 import { ATTRIBUTES } from './constants';
 import type { GlobalAttributeParams } from './types';
 
 /**
  * Checks the global params of the Attribute `<script>`.
- * @param script The `<script>` element.
+ * @param attributeKeys A record with Attribute keys that should be read from the <script> tag.
+ *
  * @returns The {@link GlobalAttributeParams}.
  */
-
-export const assessScript = (): GlobalAttributeParams => {
+export const assessScript = <AttributeKeys extends Record<string, string>>(
+  attributeKeys?: AttributeKeys
+): GlobalAttributeParams<AttributeKeys> => {
   const { currentScript } = document;
-  const { preventLoad, debugMode } = ATTRIBUTES;
+
+  const attributes = {} as GlobalAttributeParams<AttributeKeys>['attributes'];
+
+  if (!currentScript) {
+    return { attributes, preventsLoad: false };
+  }
 
   // Check if the Attribute should not be automatically loaded
-  const preventsLoad = typeof currentScript?.getAttribute(preventLoad.key) === 'string';
+  const preventsLoad = typeof currentScript.getAttribute(ATTRIBUTES.preventLoad.key) === 'string';
 
-  // Check if Debug Mode is activated
-  if (typeof currentScript?.getAttribute(debugMode.key) === 'string') Debug.activateAlerts();
+  // Retrieve <script> Attributes
+  const params: GlobalAttributeParams<AttributeKeys> = {
+    preventsLoad,
+    attributes,
+  };
 
-  return { preventsLoad };
+  for (const key in attributeKeys) {
+    const value = currentScript.getAttribute(attributeKeys[key]);
+    params.attributes[key] = value;
+  }
+
+  return params;
 };
