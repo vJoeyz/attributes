5519b1c801c63d467c9d675ad154b19f543fd2f5
Mauricio Piber <mauriciopiber@gmail.com>
2022-08-16 13:26:42 -0300

    `greenhouse` - Added description and removing all template items from cms list

--- packages/greenhouse/src/utils/populate.ts
+++ packages/greenhouse/src/utils/populate.ts
@@ -3,20 +3,24 @@ import type { Job, JobWithContent } from '@finsweet/ts-utils/dist/types/apis/Gre
 import { ATTRIBUTES, queryElement } from '../utils/constants';
 
 export function populateJob(job: Job | JobWithContent, scope: HTMLDivElement | undefined, queryParam: string | null) {
+  console.log(job);
+  // link
   const link = queryElement<HTMLLinkElement>(ATTRIBUTES.element.values.link, { scope });
 
   if (link && queryParam) {
     link.href = `${link.href}?${queryParam}=${job.id}`;
   }
 
+  // title
   const title = queryElement(ATTRIBUTES.element.values.title, { scope });
 
   if (title) {
     title.textContent = job.title;
   }
 
-  if (job.hasOwnProperty('office') || job.hasOwnProperty('departments')) {
-    const { offices, departments } = job as JobWithContent;
+  // office and department
+  if (job.hasOwnProperty('office') || job.hasOwnProperty('departments') || job.hasOwnProperty('content')) {
+    const { offices, departments, content } = job as JobWithContent;
 
     const office = queryElement(ATTRIBUTES.element.values.office, { scope });
     if (office) {
@@ -26,6 +30,12 @@ export function populateJob(job: Job | JobWithContent, scope: HTMLDivElement | u
     const department = queryElement(ATTRIBUTES.element.values.department, { scope });
     if (department) {
       department.textContent = departments[0].name || '';
+    }
+
+    const description = queryElement<HTMLElement>(ATTRIBUTES.element.values.description, { scope });
+
+    if (description) {
+      description.innerHTML = unescapeHTML(content);
     }
   }
 
@@ -37,3 +47,15 @@ export function populateJob(job: Job | JobWithContent, scope: HTMLDivElement | u
 
   return scope;
 }
+
+export const unescapeHTML = (rawHTML: string): string => {
+  return rawHTML
+    .replace(/(&nbsp;)/g, ' ')
+    .replace(/(&lt;)/g, '<')
+    .replace(/(&gt;)/g, '>')
+    .replace(/(&amp;)/g, '&')
+    .replace(/(&quot;)/g, '"')
+    .replace(/(&#96;)/g, '`')
+    .replace(/(&#x27;)/g, "'")
+    .replace(/(<br>)/g, '\n');
+};
