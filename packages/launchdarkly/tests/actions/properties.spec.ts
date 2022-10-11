6b930fbc900b93106b3b4f5c6f9b98293fe86e6f
Blessing Ekundayo <blessochampion@gmail.com>
2022-10-11 21:16:16 +0100

    Update tests

--- packages/launchdarkly/tests/actions/properties.spec.ts
+++ packages/launchdarkly/tests/actions/properties.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-const elements = `fsLaunchDarkly.extractElementsByCategory().updateElementPropertyElements`;
+const selectElement = (selector: string) => `document.querySelector('${selector}')`;
 
 test.describe('Update Element Property', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('tests/fixtures/updateElementProperty');
+    await page.goto('tests/fixtures/setProperties');
   });
 
   test.describe('When flag is not set', async () => {
     test('It does not update the property', async ({ page }) => {
       const flags = `{}`;
-      await page.evaluate(`fsLaunchDarkly.updateElementProperty(${elements}, ${flags})`);
+      const selector = '[data-test-id="image1"]';
+      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectElement(selector)}, ${flags})`);
 
-      const locator = await page.locator('data-test-id=image1');
+      const locator = await page.locator(selector);
       const src = await locator.getAttribute('src');
       await expect(src).toEqual('http://example.svg');
     });
@@ -21,13 +22,13 @@ test.describe('Update Element Property', () => {
   test.describe('When flag is set', async () => {
     const titleText = 'abc';
     const flags = `{testing: 'http://example2.svg', testing2: '${titleText}'}`;
-
-    test.beforeEach(async ({ page }) => {
-      await page.evaluate(`fsLaunchDarkly.updateElementProperty(${elements}, ${flags})`);
-    });
+    let selector: string;
 
     test('It updates src property and removes the srcset attribute', async ({ page }) => {
-      const locator = await page.locator('data-test-id=image1');
+      selector = '[data-test-id="image1"]';
+      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectElement(selector)}, ${flags})`);
+
+      const locator = await page.locator(selector);
       const src = await locator.getAttribute('src');
       await expect(src).toEqual('http://example2.svg');
 
@@ -36,7 +37,10 @@ test.describe('Update Element Property', () => {
     });
 
     test('It updates srcset property and removes the src attribute', async ({ page }) => {
-      const locator = await page.locator('data-test-id=image2');
+      selector = '[data-test-id="image2"]';
+      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectElement(selector)}, ${flags})`);
+
+      const locator = await page.locator(selector);
       const srcset = await locator.getAttribute('srcset');
       await expect(srcset).toEqual('http://example2.svg');
 
@@ -45,6 +49,9 @@ test.describe('Update Element Property', () => {
     });
 
     test('It sets the innerText of element when "setproperty" attribute is text', async ({ page }) => {
+      selector = '[data-test-id="text1"]';
+      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectElement(selector)}, ${flags})`);
+
       const locator = await page.locator('data-test-id=text1');
       const textContent = await locator.innerText();
       await expect(textContent).toEqual(titleText);
--- packages/launchdarkly/tests/actions/showOrHideElement.spec.ts
+++ packages/launchdarkly/tests/actions/showOrHideElement.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const element = `document.querySelector('[data-test-id="title-boolean"]')`;
-
-const selectorElement = (selector: string) => `document.querySelector('${selector}')`;
+const selectElement = (selector: string) => `document.querySelector('${selector}')`;
 
 test.describe('InitFlagElement for showIf attribute', () => {
   test.beforeEach(async ({ page }) => {
@@ -12,16 +10,17 @@ test.describe('InitFlagElement for showIf attribute', () => {
   test.describe('Hide Element', async () => {
     test('when flag does not exist', async ({ page }) => {
       const flags = `{}`;
-      await page.evaluate(`fsLaunchDarkly.initFlagElement(${element}, ${flags})`);
+      const selector = '[data-test-id="title1"]';
+      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectElement(selector)}, ${flags})`);
 
-      const locator = await page.locator('data-test-id=title1');
+      const locator = await page.locator(selector);
       await expect(locator).not.toBeVisible();
     });
 
     test('when flag value is not matched', async ({ page }) => {
       const flags = `{testing:"value"}`;
       const selector = '[data-test-id="title1"]';
-      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectorElement(selector)}, ${flags})`);
+      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectElement(selector)}, ${flags})`);
 
       const locator = await page.locator(selector);
       await expect(locator).not.toBeVisible();
@@ -32,7 +31,7 @@ test.describe('InitFlagElement for showIf attribute', () => {
     test('when flag exists and it is equal', async ({ page }) => {
       const flags = `{testing:"value1"}`;
       const selector = `[data-test-id="title1"]`;
-      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectorElement(selector)}, ${flags})`);
+      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectElement(selector)}, ${flags})`);
 
       const locator = await page.locator(selector);
       await expect(locator).toBeVisible();
@@ -41,7 +40,7 @@ test.describe('InitFlagElement for showIf attribute', () => {
     test('when flag exists and it is equal one of the comma separated values', async ({ page }) => {
       const flags = `{testing:"value2"}`;
       const selector = `[data-test-id="title2"]`;
-      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectorElement(selector)}, ${flags})`);
+      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectElement(selector)}, ${flags})`);
 
       const locator = await page.locator(selector);
       await expect(locator).toBeVisible();
@@ -50,7 +49,7 @@ test.describe('InitFlagElement for showIf attribute', () => {
     test('when flag is boolean and fs-launchdarkly-showif="false"', async ({ page }) => {
       const flags = `{testing:false}`;
       const selector = '[data-test-id="title-boolean"]';
-      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectorElement(selector)}, ${flags})`);
+      await page.evaluate(`fsLaunchDarkly.initFlagElement(${selectElement(selector)}, ${flags})`);
 
       const locator = await page.locator(selector);
       await expect(locator).toBeVisible();
