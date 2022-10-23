66c302433ee470a0d243b111cfa98b6e3f5516c5
Alex Iglesias <alexiglesias@finsweet.com>
2022-10-23 18:39:17 +0200

    chodehighlight: added tests

--- packages/codehighlight/tests/codehighlight.spec.ts
+++ packages/codehighlight/tests/codehighlight.spec.ts
@@ -1,18 +1,25 @@
 import { test, expect } from '@playwright/test';
-import type { Page } from '@playwright/test';
 
-/**
- * These are some demo tests to showcase Playwright.
- * You can run the tests by running `pnpm dev`.
- * If you need more info about writing tests, please visit {@link https://playwright.dev/}.
- */
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://fs-attributes.webflow.io/codehighlight');
+});
 
-// test.beforeEach(async ({ page }) => {
-//   await page.goto('https://demo.playwright.dev/todomvc');
-// });
+test.describe('codehighlight', () => {
+  test('Highlights code with hljs', async ({ page }) => {
+    const code = page.getByTestId('code-1');
+    await expect(code).toHaveClass(/hljs/);
+  });
 
-test.describe('Example', () => {
-  test('Example', async ({ page }) => {
-    //
+  test('Imports hljs theme', async ({ page }) => {
+    const link = page.locator(
+      'link[href="https://cdn.jsdelivr.net/npm/@finsweet/attributes-codehighlight@1/themes/webflow.min.css"]'
+    );
+
+    expect(await link.count()).toBeGreaterThan(0);
+  });
+
+  test('Highlights richtext component code with hljs', async ({ page }) => {
+    const codeComponent = page.locator('[fs-richtext-element] [data-testid="code-2"]');
+    await expect(codeComponent).toHaveClass(/hljs/);
   });
 });
