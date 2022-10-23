ca2b8ad097925b42658c708cf93c7379905ddb6d
Alex Iglesias <alexiglesias@finsweet.com>
2022-10-23 18:38:13 +0200

    awaitAttributesLoad: include loading resolves

--- global/factory/await.ts
+++ global/factory/await.ts
@@ -5,7 +5,9 @@
  * @param attributeKeys All the Attribute keys that must be awaited, ordered by sequence.
  */
 export const awaitAttributesLoad = async (...attributeKeys: string[]) => {
-  for (const attributeKey of attributeKeys) {
-    await window.fsAttributes[attributeKey]?.loading;
-  }
+  const loadResults = await Promise.all(
+    attributeKeys.map((attributeKey) => window.fsAttributes[attributeKey]?.loading)
+  );
+
+  return loadResults;
 };
