4960f3eed9a74601c1b5d7abb0db1423efc11020
Alex Iglesias <alexiglesias@finsweet.com>
2022-10-23 18:06:35 +0200

    a11y: created tests

---
+++ packages/a11y/tests/keyboard.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://fs-attributes.webflow.io/a11y');
+});
+
+test.describe('keyboard', () => {
+  test('Enter triggers click', async ({ page }) => {
+    const header = page.getByTestId('header-1');
+    const content = page.getByTestId('content-1');
+
+    await expect(content).toBeHidden();
+
+    await header.focus();
+    await page.keyboard.press('Enter');
+    await expect(content).toBeVisible();
+  });
+
+  test('Space triggers click', async ({ page }) => {
+    const header = page.getByTestId('header-1');
+    const content = page.getByTestId('content-1');
+
+    await expect(content).toBeHidden();
+
+    await header.focus();
+    await page.keyboard.press(' ');
+    await expect(content).toBeVisible();
+  });
+});
