29b150f56b63c59e832094e8c022c6e142ff1b75
Alex Iglesias <alexiglesias@finsweet.com>
2022-10-23 19:49:46 +0200

    mirrorinput: added tests

--- packages/mirrorinput/tests/mirrorinput.spec.ts
+++ packages/mirrorinput/tests/mirrorinput.spec.ts
@@ -1,18 +1,33 @@
-import { test, expect } from '@playwright/test';
-import type { Page } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 
-/**
- * These are some demo tests to showcase Playwright.
- * You can run the tests by running `pnpm dev`.
- * If you need more info about writing tests, please visit {@link https://playwright.dev/}.
- */
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://fs-attributes.webflow.io/mirrorinput');
+});
 
-// test.beforeEach(async ({ page }) => {
-//   await page.goto('https://demo.playwright.dev/todomvc');
-// });
+const getTriggerLocators = (page: Page) =>
+  [1, 2, 3, 4, 5].map((id) => page.locator(`[fs-mirrorinput-element="trigger-${id}"]`));
 
-test.describe('Example', () => {
-  test('Example', async ({ page }) => {
-    //
+const getTargetLocators = (page: Page) =>
+  [1, 2, 3, 4, 5].map((id) => page.locator(`[fs-mirrorinput-element="target-${id}"]`));
+
+test.describe('mirrorinput', () => {
+  test("Mirrors each trigger's input", async ({ page }) => {
+    const [trigger1, trigger2, trigger3, trigger4, trigger5] = getTriggerLocators(page);
+    const [target1, target2, target3, target4, target5] = getTargetLocators(page);
+
+    await trigger1.type('trigger 1');
+    await expect(target1).toHaveValue('trigger 1');
+
+    await trigger2.type('trigger 2');
+    await expect(target2).toHaveValue('trigger 2');
+
+    await trigger3.selectOption('First');
+    await expect(target3).toHaveValue('First');
+
+    await trigger4.check({ force: true });
+    await expect(target4).toBeChecked();
+
+    await trigger5.first().check({ force: true });
+    await expect(target5.first()).toBeChecked();
   });
 });
