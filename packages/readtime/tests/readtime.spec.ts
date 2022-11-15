7120c1488aec9a906e4d8e1f4d79d4d8fc1070b9
Alex Iglesias <alexiglesias@finsweet.com>
2022-11-15 16:03:54 +0100

    readtime: added test for multiple instances

--- packages/readtime/tests/readtime.spec.ts
+++ packages/readtime/tests/readtime.spec.ts
@@ -7,7 +7,9 @@ test.beforeEach(async ({ page }) => {
 test.describe('readtime', () => {
   test('Displays the read time', async ({ page }) => {
     const timeElement = page.locator('[fs-readtime-element="time"]');
-
     await expect(timeElement).toHaveText('4.2');
+
+    const timeElement2 = page.locator('[fs-readtime-element="time-2"]');
+    await expect(timeElement2).toHaveText('1.0');
   });
 });
