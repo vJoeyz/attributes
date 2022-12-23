d14f59c1f7a28b2c5980c6713ced9215aba58e76
Robert Kibet <kipronokrobert@gmail.com>
2022-12-23 17:42:40 +0300

    tests: show close icon accordingly
    - Combobox should show clear icon only when selected option value matches input value

--- packages/combobox/tests/combobox.spec.ts
+++ packages/combobox/tests/combobox.spec.ts
@@ -6,103 +6,103 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('combobox', () => {
-  // test('Combobox initializes properly', async ({ page }) => {
-  //   const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
-  //   const comboboxClearDropdown = page.locator('[fs-combobox-element="clear"]');
-  //   const comboboxArrowDown = page.locator('[fs-combobox-element="arrow"]');
-  //   const comboboxLabels = page.locator('[fs-combobox-element="label"]');
-  //   const comboboxInput = comboboxDropdown.locator('input');
-  //   const comboboSelect = comboboxDropdown.locator('select');
-  //   const comboboSelectOptions = comboboSelect.locator('option');
+  test('Combobox initializes properly', async ({ page }) => {
+    const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
+    const comboboxClearDropdown = page.locator('[fs-combobox-element="clear"]');
+    const comboboxArrowDown = page.locator('[fs-combobox-element="arrow"]');
+    const comboboxLabels = page.locator('[fs-combobox-element="label"]');
+    const comboboxInput = comboboxDropdown.locator('input');
+    const comboboSelect = comboboxDropdown.locator('select');
+    const comboboSelectOptions = comboboSelect.locator('option');
 
-  //   const comboboxLabelsCount = await comboboxLabels.count();
-  //   const comboboSelectOptionsCount = await comboboSelectOptions.count();
+    const comboboxLabelsCount = await comboboxLabels.count();
+    const comboboSelectOptionsCount = await comboboSelectOptions.count();
 
-  //   for (let optionIndex = 0; optionIndex < comboboSelectOptionsCount; optionIndex++) {
-  //     const option = comboboSelect.nth(optionIndex);
-  //     await expect(option).not.toBe(null);
-  //   }
+    for (let optionIndex = 0; optionIndex < comboboSelectOptionsCount; optionIndex++) {
+      const option = comboboSelect.nth(optionIndex);
+      await expect(option).not.toBe(null);
+    }
 
-  //   await expect(comboboxLabelsCount).toBeGreaterThan(0);
+    await expect(comboboxLabelsCount).toBeGreaterThan(0);
 
-  //   await expect(comboboxDropdown).toBeVisible();
-  //   await expect(comboboxClearDropdown).toHaveCSS('display', 'none');
-  //   await expect(comboboxArrowDown).toBeVisible();
-  //   await expect(comboboxInput).toBeVisible();
-  //   await expect(comboboxInput).toBeEditable();
-  // });
+    await expect(comboboxDropdown).toBeVisible();
+    await expect(comboboxClearDropdown).toHaveCSS('display', 'none');
+    await expect(comboboxArrowDown).toBeVisible();
+    await expect(comboboxInput).toBeVisible();
+    await expect(comboboxInput).toBeEditable();
+  });
 
-  // test('Combobox opens and closes by clicking on arrow.', async ({ page }) => {
-  //   const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
-  //   const comboboxArrowDown = page.locator('[fs-combobox-element="arrow"]');
+  test('Combobox opens and closes by clicking on arrow.', async ({ page }) => {
+    const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
+    const comboboxArrowDown = page.locator('[fs-combobox-element="arrow"]');
 
-  //   const comboboxNav = comboboxDropdown.locator('nav');
+    const comboboxNav = comboboxDropdown.locator('nav');
 
-  //   // open dropdown
-  //   await comboboxArrowDown.click();
-  //   await page.waitForTimeout(300);
-  //   await expect(comboboxNav).toHaveClass(/w--open/);
+    // open dropdown
+    await comboboxArrowDown.click();
+    await page.waitForTimeout(300);
+    await expect(comboboxNav).toHaveClass(/w--open/);
 
-  //   // close dropdown
-  //   await comboboxArrowDown.click();
-  //   await page.waitForTimeout(300);
-  //   await expect(comboboxNav).not.toHaveClass(/w--open/);
-  // });
+    // close dropdown
+    await comboboxArrowDown.click();
+    await page.waitForTimeout(300);
+    await expect(comboboxNav).not.toHaveClass(/w--open/);
+  });
 
-  // test('Combobox opens and closes by clicking on input.', async ({ page }) => {
-  //   const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
-  //   const comboboxInput = comboboxDropdown.locator('input');
+  test('Combobox opens and closes by clicking on input.', async ({ page }) => {
+    const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
+    const comboboxInput = comboboxDropdown.locator('input');
 
-  //   const comboboxNav = comboboxDropdown.locator('nav');
+    const comboboxNav = comboboxDropdown.locator('nav');
 
-  //   // open dropdown
-  //   await comboboxInput.click();
-  //   await page.waitForTimeout(300);
-  //   // expect nav class to include w--open
-  //   await expect(comboboxNav).toHaveClass(/w--open/);
+    // open dropdown
+    await comboboxInput.click();
+    await page.waitForTimeout(300);
+    // expect nav class to include w--open
+    await expect(comboboxNav).toHaveClass(/w--open/);
 
-  //   // close dropdown
-  //   await comboboxInput.click();
-  //   await page.waitForTimeout(300);
-  //   await expect(comboboxNav).not.toHaveClass(/w--open/);
-  // });
+    // close dropdown
+    await comboboxInput.click();
+    await page.waitForTimeout(300);
+    await expect(comboboxNav).not.toHaveClass(/w--open/);
+  });
 
-  // test('Combobox input arrow down key opens dropdown', async ({ page }) => {
-  //   const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
-  //   const comboboxInput = comboboxDropdown.locator('input');
-  //   const comboboxNav = comboboxDropdown.locator('nav');
+  test('Combobox input arrow down key opens dropdown', async ({ page }) => {
+    const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
+    const comboboxInput = comboboxDropdown.locator('input');
+    const comboboxNav = comboboxDropdown.locator('nav');
 
-  //   await comboboxInput.focus();
+    await comboboxInput.focus();
 
-  //   await comboboxInput.press('ArrowDown');
+    await comboboxInput.press('ArrowDown');
 
-  //   await expect(comboboxNav).toHaveClass(/w--open/);
-  // });
+    await expect(comboboxNav).toHaveClass(/w--open/);
+  });
 
-  // test('Combobox input on escape key if nothing has been selected, it should clear input field and close dropdown', async ({
-  //   page,
-  // }) => {
-  //   const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
-  //   const comboboxInput = comboboxDropdown.locator('input');
-  //   const comboboxClearDropdown = page.locator('[fs-combobox-element="clear"]');
+  test('Combobox input on escape key if nothing has been selected, it should clear input field and close dropdown', async ({
+    page,
+  }) => {
+    const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
+    const comboboxInput = comboboxDropdown.locator('input');
+    const comboboxClearDropdown = page.locator('[fs-combobox-element="clear"]');
 
-  //   const comboboxNav = comboboxDropdown.locator('nav');
+    const comboboxNav = comboboxDropdown.locator('nav');
 
-  //   await comboboxInput.focus();
-  //   await comboboxInput.type('test');
-  //   await comboboxInput.dispatchEvent('change');
+    await comboboxInput.focus();
+    await comboboxInput.type('test');
+    await comboboxInput.dispatchEvent('change');
 
-  //   await expect(comboboxNav).toHaveClass(/w--open/);
+    await expect(comboboxNav).toHaveClass(/w--open/);
 
-  //   await comboboxInput.press('Escape');
+    await comboboxInput.press('Escape');
 
-  //   await expect(comboboxNav).not.toHaveClass(/w--open/);
+    await expect(comboboxNav).not.toHaveClass(/w--open/);
 
-  //   const inputValue = await comboboxInput.inputValue();
-  //   await expect(inputValue).toEqual('');
+    const inputValue = await comboboxInput.inputValue();
+    await expect(inputValue).toEqual('');
 
-  //   await expect(comboboxClearDropdown).toHaveCSS('display', 'none');
-  // });
+    await expect(comboboxClearDropdown).toHaveCSS('display', 'none');
+  });
   test('Combobox input arrow down key opens dropdown and if selected option exists, it should focus on it', async ({
     page,
   }) => {
@@ -172,5 +172,42 @@ test.describe('combobox', () => {
     const inputFieldActiveDescendant2 = await comboboxInput.getAttribute('aria-activedescendant');
 
     expect(inputFieldActiveDescendant2).toEqual(selectedOptionId2);
+  });
+
+  test('Combobox should show clear icon only when selected option value matches input value', async ({ page }) => {
+    const comboboxDropdown = page.locator('[fs-combobox-element="dropdown"]');
+    const comboboxInput = comboboxDropdown.locator('input');
+    const comboboxNav = comboboxDropdown.locator('nav');
+    const comboboxOptions = comboboxNav.locator('a');
+    const comboboxClearDropdown = page.locator('[fs-combobox-element="clear"]');
+
+    await expect(comboboxClearDropdown).toHaveCSS('display', 'none');
+
+    await comboboxInput.click();
+
+    expect(await comboboxNav).toHaveClass(/w--open/);
+
+    const randomOptionIndex = Math.floor(Math.random() * (await comboboxOptions.count()));
+    const randomOption = comboboxOptions.nth(randomOptionIndex);
+    await randomOption.click();
+
+    await expect(comboboxNav).not.toHaveClass(/w--open/);
+
+    await expect(comboboxClearDropdown).toBeVisible();
+
+    const activeElement = await page.evaluate(() => document.activeElement?.tagName);
+    await expect(activeElement).toEqual('INPUT');
+
+    await comboboxInput.press('Backspace');
+    await expect(comboboxClearDropdown).toHaveCSS('display', 'none');
+
+    await comboboxInput.press('Backspace');
+    await expect(comboboxClearDropdown).toHaveCSS('display', 'none');
+
+    await comboboxInput.fill('');
+    await expect(comboboxClearDropdown).toHaveCSS('display', 'none');
+
+    await comboboxInput.press('Escape');
+    await expect(comboboxClearDropdown).toHaveCSS('display', 'flex');
   });
 });
