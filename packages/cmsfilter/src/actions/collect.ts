830e88addf11dde5948cb908dd123774edb2291a
Robert Kibet <kipronokrobert@gmail.com>
2023-02-21 16:28:52 +0300

    refactor: add clear attribute inplace of reset

--- packages/cmsfilter/src/actions/collect.ts
+++ packages/cmsfilter/src/actions/collect.ts
@@ -47,10 +47,14 @@ export const collectFiltersElements = (
   const form = formBlock.querySelector('form') as HTMLFormElement;
   const submitButton = formBlock.querySelector<HTMLInputElement>('input[type="submit"]');
 
-  const resetButtonElements = formBlock.querySelectorAll<HTMLElement>(
-    getSelector('element', 'reset', { operator: 'prefixed' })
-  );
+  const resetButtonElements = [
+    formBlock.querySelectorAll<HTMLElement>(getSelector('element', 'reset', { operator: 'prefixed' })),
+    formBlock.querySelectorAll<HTMLElement>(getSelector('element', 'resetFallback', { operator: 'prefixed' })),
+  ].find((resetButtons) => resetButtons.length > 0);
+
   const resetButtonsData: ResetButtonsData = new Map();
+
+  if (!resetButtonElements) return { form, submitButton, resetButtonsData };
 
   for (const resetButton of resetButtonElements) {
     const rawFilterKeys = resetButton.getAttribute(resetKey);
