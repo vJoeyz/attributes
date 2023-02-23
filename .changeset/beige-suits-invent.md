75923414f140557072a14eddffb237b75c677a01
Alex Iglesias <alexiglesias@finsweet.com>
2023-02-23 16:04:12 +0100

    numbercount: added new `fs-numbercount-locale="{LOCALE} | auto"` Attribute.

---
+++ .changeset/beige-suits-invent.md
@@ -0,0 +1,7 @@
+---
+'@finsweet/attributes-numbercount': minor
+---
+
+feat: added new `fs-numbercount-locale="{LOCALE} | auto"` Attribute.
+If defined, the library will format the number using the provided locale.
+Users can define any [BCP 47 language tag](https://www.techonthenet.com/js/language_tags.php) or use `auto` to use the browser's locale.
--- packages/numbercount/src/factory.ts
+++ packages/numbercount/src/factory.ts
@@ -5,7 +5,7 @@ import { parseNumericAttribute } from '$global/helpers';
 import { setNumberCountA11Y } from './actions/a11y';
 import { animateNumberCount } from './actions/animate';
 import { observeIntersection } from './actions/observe';
-import { DEFAULT_DURATION, DEFAULT_START_NUMBER, DEFAULT_THRESHOLD, getAttribute } from './utils/constants';
+import { ATTRIBUTES, DEFAULT_DURATION, DEFAULT_START_NUMBER, DEFAULT_THRESHOLD, getAttribute } from './utils/constants';
 import { extractNumberFromElement } from './utils/helpers';
 
 /**
@@ -17,6 +17,7 @@ export const initNumberCount = (numberElement: Element) => {
   const rawEnd = getAttribute(numberElement, 'end');
   const rawDuration = getAttribute(numberElement, 'duration');
   const rawThreshold = getAttribute(numberElement, 'threshold');
+  const rawLocale = getAttribute(numberElement, 'locale');
 
   const end = parseNumericAttribute(rawEnd, extractNumberFromElement(numberElement));
   if (!isNumber(end)) return;
@@ -24,10 +25,11 @@ export const initNumberCount = (numberElement: Element) => {
   const start = parseNumericAttribute(rawStart, DEFAULT_START_NUMBER);
   const duration = parseNumericAttribute(rawDuration, DEFAULT_DURATION);
   const threshold = parseNumericAttribute(rawThreshold, DEFAULT_THRESHOLD);
+  const locale = rawLocale ? (rawLocale === ATTRIBUTES.locale.values.auto ? true : rawLocale) : undefined;
 
   setNumberCountA11Y(numberElement, start, end);
 
   return observeIntersection(numberElement, threshold, () => {
-    animateNumberCount(numberElement, start, end, duration);
+    animateNumberCount(numberElement, start, end, duration, locale);
   });
 };
