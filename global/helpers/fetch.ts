134a2150796402cceca34f24d06a11cb9d91365e
Alex Iglesias <alexiglesias@finsweet.com>
2023-02-24 11:57:42 +0100

    `global`: prevent blocking IndexedDB transactions when other browser tabs request the same DB.

--- global/helpers/fetch.ts
+++ global/helpers/fetch.ts
@@ -8,43 +8,49 @@ const memoryCache = new Map<string, Promise<string>>();
  * Stores the page response in a {@link IDBDatabase} if the page belongs to the same site.
  *
  * @param source The URL of the page.
+ * @param cacheExternalDocuments Whether to cache external documents.
+ * If set to true, it will follow a [stale-while-revalidate](https://web.dev/stale-while-revalidate/) strategy.
  *
  * @returns The page's {@link Document} if successful, `null` otherwise.
  */
-export const fetchPageDocument = async (source: string | URL) => {
+export const fetchPageDocument = async (
+  source: string | URL,
+  cacheExternalDocuments?: boolean
+): Promise<Document | null> => {
   try {
     const url = new URL(source, window.location.origin);
+
+    // If the same page is being fetched simultaneously, return it from the memory cache.
+    const pageFromMemory = await getPageFromMemory(url);
+    if (pageFromMemory) return pageFromMemory;
 
     // Try to create a DB instance.
     const siteId = getSiteId();
     const publishDate = getPublishDate();
     const db = siteId && publishDate ? await createCacheDB(siteId, publishDate) : null;
 
-    // If the same page is being fetched simultaneously, get it from the memory cache.
-    let rawPagePromise = memoryCache.get(url.href);
-    if (rawPagePromise) return parseRawPage(await rawPagePromise);
-
-    // If the page is in the DB, get it from there.
-    let rawPage = db ? await getPageFromDB(db, url.href) : null;
-    if (rawPage) return parseRawPage(rawPage);
-
-    // Get the raw page promise and store it in the memory cache.
-    rawPagePromise = fetchRawPage(url.href);
-
-    memoryCache.set(url.href, rawPagePromise);
-
-    // Get the page.
-    rawPage = await rawPagePromise;
-
-    const page = parseRawPage(rawPage);
-    const fetchedSiteId = getSiteId(page);
-
-    // If the page belongs to the same site, store it in the DB.
-    if (db && fetchedSiteId && fetchedSiteId === siteId) {
-      await storePageInDB(db, url.href, rawPage);
-      memoryCache.delete(url.href);
+    // If no DB, fetch the page and store it in the memory cache.
+    if (!db || !siteId) {
+      const { page } = await fetchAndCachePageInMemory(url);
+      return page;
     }
 
+    // If the page is in the DB, return it from there.
+    const rawPageFromDB = await getRawPageFromDB(db, url.href);
+    if (rawPageFromDB) {
+      const page = parseRawPage(rawPageFromDB);
+
+      // Cached external documents are considered stale,
+      // so in the background we refetch the page and store it to the DB again.
+      if (cacheExternalDocuments && !isPageSameSite(page, siteId)) {
+        fetchAndCachePageInDB(db, url, siteId, cacheExternalDocuments);
+      }
+
+      return page;
+    }
+
+    // If the page is not in the DB, fetch it and store it in the DB.
+    const page = await fetchAndCachePageInDB(db, url, siteId, cacheExternalDocuments);
     return page;
   } catch (err) {
     return null;
@@ -52,12 +58,62 @@ export const fetchPageDocument = async (source: string | URL) => {
 };
 
 /**
- * Fetches a page and returns the HTML text.
- * @param href
+ * @returns A page from the memory cache, if existing.
+ * @param url The URL of the page.
  */
-const fetchRawPage = async (href: string) => {
-  const response = await fetch(href);
-  return response.text();
+const getPageFromMemory = async (url: URL) => {
+  const rawPagePromise = await memoryCache.get(url.href);
+  if (rawPagePromise) {
+    return parseRawPage(rawPagePromise);
+  }
+};
+
+/**
+ * Fetches a page and stores it in memory.
+ * @param url The URL of the page.
+ * @returns The page's {@link Document} and raw HTML text.
+ */
+const fetchAndCachePageInMemory = async (url: URL) => {
+  const rawPagePromise = fetch(url.href).then((response) => response.text());
+
+  memoryCache.set(url.href, rawPagePromise);
+
+  const rawPage = await rawPagePromise;
+  const page = parseRawPage(rawPage);
+  return { page, rawPage };
+};
+
+/**
+ * Fetches a page and stores it in a {@link IDBDatabase}.
+ * @param db The DB instance.
+ * @param url The URL of the page.
+ * @param siteId The current site ID.
+ * @param cacheExternalDocuments Whether to cache external documents.
+ * @returns The page's {@link Document}.
+ */
+const fetchAndCachePageInDB = async (db: IDBDatabase, url: URL, siteId: string, cacheExternalDocuments?: boolean) => {
+  const { page, rawPage } = await fetchAndCachePageInMemory(url);
+
+  const isSameSite = isPageSameSite(page, siteId);
+
+  // If it can't be cached, just return the page.
+  if (!isSameSite && !cacheExternalDocuments) return page;
+
+  // Otherwise store it in the DB
+  await storeRawPageInDB(db, url.href, rawPage);
+
+  // If the page belongs to the same site, we can remove it from the memory cache as it's already in the DB.
+  // If the page is external, we want to keep it in memory to avoid refetching it, as in every request is considered stale.
+  if (isSameSite) {
+    memoryCache.delete(url.href);
+  }
+
+  return page;
+};
+
+const isPageSameSite = (page: Document, siteId: string) => {
+  const pageSiteId = getSiteId(page);
+  return pageSiteId && pageSiteId === siteId;
 };
 
 /**
@@ -73,29 +129,36 @@ const parseRawPage = (rawPage: string) => new DOMParser().parseFromString(rawPag
  * @returns An {@link IDBDatabase} instance.
  */
 const createCacheDB = (siteId: string, publishDate: Date) => {
-  try {
-    const dbOpenRequest = window.indexedDB.open(siteId, publishDate.getTime());
+  return new Promise<IDBDatabase | null>((resolve) => {
+    try {
+      const dbOpenRequest = window.indexedDB.open(siteId, publishDate.getTime());
 
-    dbOpenRequest.onupgradeneeded = () => {
-      const db = dbOpenRequest.result;
+      dbOpenRequest.onblocked = () => {
+        resolve(null);
+      };
 
-      if (db.objectStoreNames.contains(DB_OBJECT_STORE_NAME)) {
-        db.deleteObjectStore(DB_OBJECT_STORE_NAME);
-      }
+      dbOpenRequest.onupgradeneeded = () => {
+        const db = dbOpenRequest.result;
 
-      db.createObjectStore(DB_OBJECT_STORE_NAME);
-    };
+        if (db.objectStoreNames.contains(DB_OBJECT_STORE_NAME)) {
+          db.deleteObjectStore(DB_OBJECT_STORE_NAME);
+        }
 
-    return new Promise<IDBDatabase | null>((resolve) => {
+        db.createObjectStore(DB_OBJECT_STORE_NAME);
+      };
+
       dbOpenRequest.onerror = () => resolve(null);
       dbOpenRequest.onsuccess = () => {
         const db = dbOpenRequest.result;
+
+        db.onversionchange = () => db.close();
+
         resolve(db);
       };
-    });
-  } catch (err) {
-    return null;
-  }
+    } catch (err) {
+      resolve(null);
+    }
+  });
 };
 
 /**
@@ -103,7 +166,7 @@ const createCacheDB = (siteId: string, publishDate: Date) => {
  * @param db
  * @param href
  */
-const getPageFromDB = async (db: IDBDatabase, href: string) => {
+const getRawPageFromDB = async (db: IDBDatabase, href: string) => {
   return new Promise<string | null>((resolve) => {
     const transaction = db.transaction(DB_OBJECT_STORE_NAME);
     const objectStore = transaction.objectStore(DB_OBJECT_STORE_NAME);
@@ -120,7 +183,7 @@ const getPageFromDB = async (db: IDBDatabase, href: string) => {
  * @param href
  * @param rawPage
  */
-const storePageInDB = async (db: IDBDatabase, href: string, rawPage: string) => {
+const storeRawPageInDB = async (db: IDBDatabase, href: string, rawPage: string) => {
   return new Promise<void>((resolve) => {
     const transaction = db.transaction(DB_OBJECT_STORE_NAME, 'readwrite');
     const objectStore = transaction.objectStore(DB_OBJECT_STORE_NAME);
