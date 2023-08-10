40bf65bbe8fcd4a28cf4061d3a0755b15b779276
Dovbaka <dovbaka99@gmail.com>
2023-08-10 11:44:31 +0300

    lint fix

--- packages/masonry/src/utils/constants.ts
+++ packages/masonry/src/utils/constants.ts
@@ -1,7 +1,5 @@
 import { type AttributeElements, type AttributeSettings } from '@finsweet/attributes-utils';
 
-export const ELEMENTS = [
-  'list',
-] as const satisfies AttributeElements;
+export const ELEMENTS = ['list'] as const satisfies AttributeElements;
 
 export const SETTINGS = {} as const satisfies AttributeSettings;
