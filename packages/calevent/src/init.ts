0b8b59be32af2fe763f8ee875e6ebb208fa17a21
Michael Gatuma <50529359+michaelgatuma@users.noreply.github.com>
2023-09-23 18:30:17 +0300

    feat: handle cms lists

--- packages/calevent/src/init.ts
+++ packages/calevent/src/init.ts
@@ -1,4 +1,5 @@
-import { type FsAttributeInit, waitWebflowReady } from '@finsweet/attributes-utils';
+import type { CMSList } from '@finsweet/attributes-cmscore';
+import { type FsAttributeInit, waitAttributeLoaded, waitWebflowReady } from '@finsweet/attributes-utils';
 
 import { listenTriggerClicks } from './actions/trigger';
 import { createCalendarEventInstances } from './factory';
@@ -11,7 +12,16 @@ export const init: FsAttributeInit = async () => {
 
   listenTriggerClicks();
 
+  // create button for static items
   createCalendarEventInstances();
 
   return {};
+  // create button from dynamic list in memory
+  const listInstances: CMSList[] = (await waitAttributeLoaded('cmsload')) || [];
+
+  for (const { items } of listInstances) {
+    for (const { element } of items) {
+      createCalendarEventInstances(element);
+    }
+  }
 };
