4c8f776343f862f41fecd49bece4bf306171a7c2
Alex Iglesias <alexiglesias@finsweet.com>
2025-04-24 15:13:53 +0200

    fix: ignore empty filters when filtering

--- packages/list/src/filter/filter.worker.ts
+++ packages/list/src/filter/filter.worker.ts
@@ -1,23 +1,50 @@
 import { extractCommaSeparatedValues } from '@finsweet/attributes-utils';
-
 import type { FieldValue, FiltersCondition, FiltersGroup, FilterTaskData } from './types';
 import { areEqual, numericCompare, parseFilterValue } from './utils';
+
+type ValidFiltersGroup = FiltersGroup & {
+  validConditions: FiltersCondition[];
+};
 
 self.onmessage = (e: MessageEvent<FilterTaskData>) => {
   const { filters, items } = e.data;
 
+  // Filter out invalid conditions (e.g. empty values) and groups (e.g. no valid conditions)
+  const validGroups = filters.groups.reduce<ValidFiltersGroup[]>((validGroups, group) => {
+    const validConditions = group.conditions.reduce<FiltersCondition[]>((validConditions, condition) => {
+      const { op, value, fieldKey } = condition;
+      if (!fieldKey) return validConditions;
+      if (!op) return validConditions;
+
+      if (op !== 'empty' && op !== 'not-empty') {
+        const valueIsEmpty = value === '' || (Array.isArray(value) && !value.length);
+        if (valueIsEmpty) return validConditions;
+      }
+
+      validConditions.push(condition);
+
+      return validConditions;
+    }, []);
+
+    if (validConditions.length) {
+      validGroups.push({ ...group, validConditions });
+    }
+
+    return validGroups;
+  }, []);
+
+  // Filter the items based on the valid groups and conditions
   const filteredItems = items.filter((item) => {
-    const groupsPredicate = (groupData: FiltersGroup) => {
-      const conditionsPredicate = (filterData: FiltersCondition) => {
+    const groupsPredicate = (groupData: ValidFiltersGroup) => {
+      const conditionsPredicate = (condition: FiltersCondition) => {
         const fieldsPredicate = (fieldKey: string) => {
-          const { op, filterMatch, fieldMatch } = filterData;
-          if (!op) return true;
+          const { op, filterMatch, fieldMatch } = condition;
 
           const fieldData = item.fields[fieldKey];
           if (!fieldData) return false;
 
           const fieldValue = fieldData.value;
-          const filterValue = filterData.value;
+          const filterValue = condition.value;
 
           if (op === 'empty') {
             return Array.isArray(fieldValue) ? !fieldValue.length : !fieldValue;
@@ -27,13 +54,13 @@ self.onmessage = (e: MessageEvent<FilterTaskData>) => {
             return Array.isArray(fieldValue) ? !!fieldValue.length : !!fieldValue;
           }
 
-          const isNegative = op.startsWith('not-');
+          const isNegative = op?.startsWith('not-');
 
           const compare = (fieldValue: FieldValue, filterValue: string, parsedFilterValue: FieldValue) => {
             let match = false;
 
             if (op === 'equal' || op === 'not-equal') {
-              match = areEqual(fieldValue, parsedFilterValue, filterData.fuzzyThreshold);
+              match = areEqual(fieldValue, parsedFilterValue, condition.fuzzyThreshold);
             } else if (op === 'start' || op === 'not-start') {
               match = fieldValue.toString().toLowerCase().startsWith(filterValue.toLowerCase());
             } else if (op === 'end' || op === 'not-end') {
@@ -54,16 +81,14 @@ self.onmessage = (e: MessageEvent<FilterTaskData>) => {
 
           const filterValueIsArray = Array.isArray(filterValue);
           const fieldValueIsArray = Array.isArray(fieldValue);
-          const emptyFilterValue = filterValue === '' || (filterValueIsArray && !filterValue.length);
-          const emptyFieldValue = fieldValue === '' || (fieldValueIsArray && !fieldValue.length);
 
-          if (emptyFilterValue) return groupData.conditionsMatch === 'and';
+          const emptyFieldValue = fieldValue === '' || (fieldValueIsArray && !fieldValue.length);
           if (emptyFieldValue) return isNegative;
 
           // Both are arrays
           if (filterValueIsArray && fieldValueIsArray) {
             const predicate = (filterValue: string) => {
-              const parsedFilterValue = parseFilterValue(filterValue, filterData.type, fieldData.type);
+              const parsedFilterValue = parseFilterValue(filterValue, condition.type, fieldData.type);
               if (parsedFilterValue === null) return false;
 
               return fieldMatch === 'and'
@@ -77,7 +102,7 @@ self.onmessage = (e: MessageEvent<FilterTaskData>) => {
           // Filter is array, field is single
           if (filterValueIsArray && !fieldValueIsArray) {
             const predicate = (filterValue: string) => {
-              const parsedFilterValue = parseFilterValue(filterValue, filterData.type, fieldData.type);
+              const parsedFilterValue = parseFilterValue(filterValue, condition.type, fieldData.type);
               if (parsedFilterValue === null) return false;
 
               return compare(fieldValue, filterValue, parsedFilterValue);
@@ -88,7 +113,7 @@ self.onmessage = (e: MessageEvent<FilterTaskData>) => {
 
           // Filter is single, field is array
           if (!filterValueIsArray && fieldValueIsArray) {
-            const parsedFilterValue = parseFilterValue(filterValue, filterData.type, fieldData.type);
+            const parsedFilterValue = parseFilterValue(filterValue, condition.type, fieldData.type);
             if (parsedFilterValue === null) return false;
 
             return fieldMatch === 'and'
@@ -98,7 +123,7 @@ self.onmessage = (e: MessageEvent<FilterTaskData>) => {
 
           // Filter is single, field is single
           if (!filterValueIsArray && !fieldValueIsArray) {
-            const parsedFilterValue = parseFilterValue(filterValue, filterData.type, fieldData.type);
+            const parsedFilterValue = parseFilterValue(filterValue, condition.type, fieldData.type);
             if (parsedFilterValue === null) return false;
 
             return compare(fieldValue, filterValue, parsedFilterValue);
@@ -109,19 +134,17 @@ self.onmessage = (e: MessageEvent<FilterTaskData>) => {
         };
 
         const fieldKeys =
-          filterData.fieldKey === '*'
-            ? Object.keys(items[0]?.fields || {})
-            : extractCommaSeparatedValues(filterData.fieldKey);
+          condition.fieldKey === '*' ? Object.keys(item.fields) : extractCommaSeparatedValues(condition.fieldKey);
 
-        return filterData.fieldMatch === 'and' ? fieldKeys.every(fieldsPredicate) : fieldKeys.some(fieldsPredicate);
+        return condition.fieldMatch === 'and' ? fieldKeys.every(fieldsPredicate) : fieldKeys.some(fieldsPredicate);
       };
 
       return groupData.conditionsMatch === 'or'
-        ? groupData.conditions.some(conditionsPredicate)
-        : groupData.conditions.every(conditionsPredicate);
+        ? groupData.validConditions.some(conditionsPredicate)
+        : groupData.validConditions.every(conditionsPredicate);
     };
 
-    return filters.groupsMatch === 'or' ? filters.groups.some(groupsPredicate) : filters.groups.every(groupsPredicate);
+    return filters.groupsMatch === 'or' ? validGroups.some(groupsPredicate) : validGroups.every(groupsPredicate);
   });
 
   self.postMessage(filteredItems);
