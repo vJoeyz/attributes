52ef93ac668a2d43419d710f912382077df78bee
Alex Iglesias <alexiglesias@finsweet.com>
2025-05-08 14:25:31 +0200

    remove unused normalizePropKey

--- packages/utils/src/helpers/strings.ts
+++ packages/utils/src/helpers/strings.ts
@@ -1,17 +1,4 @@
 /**
- * Normalizes a property key.
- * - Trims the string.
- * - Lowercases the string.
- *
- * @param propKey The property key to normalize.
- */
-export function normalizePropKey(propKey: string): string;
-export function normalizePropKey(propKey?: string | null): string | undefined;
-export function normalizePropKey(propKey?: string | null): string | undefined {
-  return propKey?.trim().toLowerCase();
-}
-
-/**
  * Convert a string of comma separated values to an array of values.
  *
  * @param string Comma separated string.
